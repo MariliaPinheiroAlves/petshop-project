0b904a7190d17a5d49da30f5f3ea7f999c9aad6c
Marilia Pinheiro Alves <mariliapinheiroalves@gmail.com>
2024-08-18 20:12:10 -0300

    Linkando atendimentos e finalizando construção da função exibirAtendimentos

--- atendimentos/atendimentos.js
+++ atendimentos/atendimentos.js
@@ -12,61 +12,90 @@ const atendimento = [
     observacao: null,
     statusDoAtendimento: "Finalizado"
   },
-  { 
-    nomeDoCachorro: "Malu", 
-    raca: "RND", 
-    funcionarioResponsavel: "João", 
+  {
+    nomeDoCachorro: "Malu",
+    raca: "RND",
+    funcionarioResponsavel: "João",
     porte: "Pequeno",
     sexo: "Femêa",
     especie: "Canina",
     pelagem: "Curto",
     observacao: null,
-    statusDoAtendimento: "Em Andamento" 
+    statusDoAtendimento: "Em Andamento"
   },
-  { 
-    nomeDoCachorro: "Teka", 
-    raca: "Pug", 
-    funcionarioResponsavel: "Marcos", 
+  {
+    nomeDoCachorro: "Teka",
+    raca: "Pug",
+    funcionarioResponsavel: "Marcos",
     porte: "Pequeno",
     sexo: "Femêa",
     especie: "Canina",
     pelagem: "Curto",
     observacao: "Animal com problema dermatologico",
-    statusDoAtendimento: "Finalizado" 
+    statusDoAtendimento: "Finalizado"
   },
-  { 
-    nomeDoCachorro: "Paçoca", 
-    raca: "Golden", 
-    funcionarioResponsavel: "Marcos", 
+  {
+    nomeDoCachorro: "Paçoca",
+    raca: "Golden",
+    funcionarioResponsavel: "Marcos",
     porte: "Grande",
     sexo: "Macho",
     especie: "Canina",
     pelagem: "Longo",
     observacao: null,
-    statusDoAtendimento: "Em Andamento" 
+    statusDoAtendimento: "Em Andamento"
   },
-  { 
-    nomeDoCachorro: "Pipoca", 
-    raca: "Rottweiler", 
-    funcionarioResponsavel: "Carlos", 
+  {
+    nomeDoCachorro: "Pipoca",
+    raca: "Rottweiler",
+    funcionarioResponsavel: "Carlos",
     porte: "Grande",
     sexo: "Femêa",
     especie: "Canina",
     pelagem: "Curto",
     observacao: "Animal feroz",
-    statusDoAtendimento: "Em Andamento" 
+    statusDoAtendimento: "Em Andamento"
   },
 ]
 
 const exibirAtendimentos = () => {
   for (let i = 0; i < atendimento.length; i++) {
     atendimentos.innerHTML +=
-      `<div class="atendimento">
-          <p>Nome do Pet: ${atendimento[i].nomeDoCachorro}</p>
-          <p>Raça: ${atendimento[i].raca}</p>
-          <p>Funcionario: ${atendimento[i].funcionarioResponsavel}</p> 
-          <p>Status: ${atendimento[i].statusDoAtendimento}</p>
+      `
+      <div class="col-12 col-md-10 d-flex flex-column justify-content-center align-items-center mb-4">
+        <p class="w-100">Atendimento ${i+1}</p>
+        <div class="container d-flex align-items-center justify-content-around border rounded border-black p-2 mb-2">
+          <div class="d-flex justify-content-between align-items-center w-75">
+            <p><span class="fw-bold">Nome do Pet:</span> Jujuba</p>
+            <p><span class="fw-bold">Raça:</span> Dachshund</p>
+            <p><span class="fw-bold">Porte:</span> Pequeno</p>
+          </div>
+          <a href="#" class="btn">Buscar</a>
         </div>
+
+        <div class="container border rounded border-black p-2 row">
+          <div class="col-9">
+            <div class="d-flex justify-content-evenly my-3">
+              <p><span class="fw-bold">Nome do Pet:</span> Jujuba</p>
+              <p><span class="fw-bold">Espécie:</span> Canina</p>
+              <p><span class="fw-bold">Raça:</span> Dachshund</p>
+            </div>
+            <div class="d-flex justify-content-evenly mb-3">
+              <p><span class="fw-bold">Sexo:</span> Fêmea</p>
+              <p><span class="fw-bold">Pelagem:</span> Pelo curto</p>
+              <p><span class="fw-bold">Porte:</span> Pequeno</p>
+            </div>
+            <div>
+              <p><span class="fw-bold">Funcionário Responsável:</span> Marília</p>
+              <p><span class="fw-bold">Observações:</span> Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Odio, asperiores?</p>
+            </div>
+          </div>
+          <div class="col-3">
+            <img src="https://placehold.co/200x200" alt="" class="img-fluid">
+          </div>
+        </div>
+      </div>
       `
   }
 }
